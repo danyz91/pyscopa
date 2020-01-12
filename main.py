54d586daa03dcac0615f8e06cc9d186ee4caaee0
danyz91 <d.romano991@gmail.com>
2020-01-12 20:24:18 +0100

    added argparse to main

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from cardgames.scopa import Scopa
+import argparse
+
+parser = argparse.ArgumentParser(description='PyScopa launcher')
+
+parser.add_argument('-g', '--gui', help='Enable gui', action='store_true')
+parser.add_argument('-p', '--human', help='Enable Human player', action='store_true')
+parser.add_argument('-n', '--n_players', type=int, help='Number of players', default=2, choices=(2, 3, 4))
 
 
 def main():
-    game = Scopa(gui=False)
-    game.init_game(n_players=4, human=False)
+    args = parser.parse_args()
+    game = Scopa(gui=args.gui)
+    game.init_game(n_players=args.n_players, human=args.human)
     game.start_game()
 
 
